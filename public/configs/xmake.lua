7137b0582935dc8a17e89e8b43982172e5c43f4c
Extra-Creativity <liangjiaming@stu.pku.edu.cn>
2024-02-02 09:05:46 +0800

    Remove msvc options.

--- public/configs/xmake.lua
+++ public/configs/xmake.lua
@@ -4,6 +4,4 @@ target("mini_lisp")
   add_files("src/*.cpp")
   set_languages("c++20")
   set_targetdir("bin")
-  if is_plat("windows") then
-    add_cxflags("/utf-8", "/Zc:preprocessor")
-  end
+  set_encodings("utf-8")
